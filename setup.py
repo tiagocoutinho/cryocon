b342fc9d33dc4e763db7a1678db2d911d808ce67
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-13 11:36:39 +0200

    Bump version: 3.1.8 → 3.1.9

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='cryocon',
     author="Jairo Moldes",
     author_email='jmoldes@cells.es',
-    version='3.1.8',
+    version='3.1.9',
     description="CryCon library",
     long_description=description,
     long_description_content_type="text/markdown",
