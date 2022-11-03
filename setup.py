0ee2714ece228b17e1192274e3c3ede97dd4fa1f
Roberto Javier Homs Puron <rhoms@cells.es>
2022-11-03 15:31:29 +0100

    Bump version: 3.1.11 → 3.1.12

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='cryocon',
     author="Jairo Moldes",
     author_email='jmoldes@cells.es',
-    version='3.1.11',
+    version='3.1.12',
     description="CryCon library",
     long_description=description,
     long_description_content_type="text/markdown",
